bf4f26d879387ab0405bf818b5e31a4cc6042d2c
Kill-confirmed <54537856+Kill-confirmed@users.noreply.github.com>
2020-10-27 13:39:04 -0500

    Rocket direction

---
+++ Assets/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour{
+    Rigidbody rigidBody;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        rigidBody=GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ProcessInput();
+    }
+
+    private void ProcessInput(){
+       // GetKey applies all the time and will report tge status of the named key.
+        if (Input.GetKey(KeyCode.Space)){
+            print("Thrust");
+            rigidBody.AddRelativeForce(Vector3.up);
+          
+        }
+        // Rotate left or right
+        if (Input.GetKey(KeyCode.A)){
+            // print("Rotate Left");
+            transform.Rotate(Vector3.forward);
+        }
+
+        else if (Input.GetKey(KeyCode.D)){
+            // print("Rotate Right");
+            transform.Rotate(-Vector3.forward);
+        }
+        
+
+    }
+}
